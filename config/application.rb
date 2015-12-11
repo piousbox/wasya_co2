859cfc2642d2bef9143776e25c6903d738ba86a4
Victor Piousbox <piousbox@gmail.com>
2015-12-11 15:59:25 -0800

    up to date

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module WasyaCo3
   class Application < Rails::Application
+    paths['public'] = File.join( Rails.root, "public_rails" )
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
