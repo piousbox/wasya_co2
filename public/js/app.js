6de3a54f121a77adf1d9141417fa516d3cf349cd
Victor Piousbox <piousbox@gmail.com>
2015-12-23 05:27:44 -0800

    removed some js logging

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,10 @@
 $(document).ready(function() {
 
   $("a.about").click(function() {
-    console.log('clicked');
     $.scrollTo( ".about-wasya-co", 1000 ); // , {easing:'elasout'} );
   });
 
   $("a.services").click(function() {
-    console.log('clicked');
     $.scrollTo( ".services-list", 1000 );
   });
 
